bd140a7c2cc27c9057f84b2657d61901c8e54a7b
ghigbie <gehigbie@gmail.com>
2018-02-24 20:05:55 -0700

    Add new component

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,33 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var Header = function Header() {
+    var title = 'Indecison App';
+    var subTitle = 'Put your life in the hands of a computer...';
+    var style = {
+        backgroundColor: '#232323',
+        textAlign: 'center',
+        color: '#c4c4c4'
+    };
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Stuff = function (_React$Component) {
-    _inherits(Stuff, _React$Component);
-
-    function Stuff() {
-        _classCallCheck(this, Stuff);
-
-        return _possibleConstructorReturn(this, (Stuff.__proto__ || Object.getPrototypeOf(Stuff)).apply(this, arguments));
-    }
-
-    _createClass(Stuff, [{
-        key: 'render',
-        value: function render() {
-            return React.createElement(
+    return React.createElement(
+        'div',
+        { style: style },
+        React.createElement(
+            'div',
+            { className: 'container' },
+            React.createElement(
                 'h1',
                 null,
-                'Yo!'
-            );
-        }
-    }]);
-
-    return Stuff;
-}(React.Component);
+                title
+            ),
+            React.createElement(
+                'h3',
+                null,
+                subTitle
+            )
+        )
+    );
+};
 
 var appRoot = document.getElementById('react-container');
-ReactDOM.render(React.createElement(Stuff, null), appRoot);
+ReactDOM.render(React.createElement(Header, null), appRoot);
--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 
-class Stuff extends React.Component{
-    render(){
-        return(
-            <h1>Yo!</h1>
-        )
+
+const Header = () => {
+    const title = 'Indecison App';
+    const subTitle = 'Put your life in the hands of a computer...'
+    const style = {
+        backgroundColor: '#232323',
+        textAlign: 'center',
+        color: '#c4c4c4'
     }
+
+    return(
+        <div style={style}>
+            <div className="container">
+                <h1>{title}</h1>
+                <h3>{subTitle}</h3>
+            </div>
+        </div>
+    )
 }
 
+
 const appRoot = document.getElementById('react-container');
-ReactDOM.render(<Stuff />, appRoot);
+ReactDOM.render(<Header/>, appRoot);
